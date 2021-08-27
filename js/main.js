c5db196d6c370b049a997dddc2bc6620a10438ef
eduardoltc <eduaardolucaas@gmail.com>
2021-08-26 23:43:33 -0300

    modificando

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 // Função para consultar endereços através do CEP informado.
 
@@ -24,31 +23,4 @@ function consultaCep() {
             //document.getElementById("uf").innerHTML = "Estado: " +response.uf;
         }
     })
-=======
-
-// Função para consultar endereços através do CEP informado.
-
-function consultaCep(){
-    var cep = document.getElementById("cep").value;
-    var url = "https://viacep.com.br/ws/" + cep + "/json/";
-    console.log(url);
-    $.ajax({
-        url: url,
-        type: "GET",
-        success: function(response){
-            console.log(response);
-            $("#logradouro").html("Logradouro: " +response.logradouro);
-            $("#bairro").html("Bairro: " + response.bairro);
-            $("#localidade").html("Cidade: " + response.localidade);
-            $("#uf").html("Estado: " + response.uf);
-
-            // Outra forma
-
-            //document.getElementById("logradouro").innerHTML = "Logradouro: " + response.logradouro;
-            //document.getElementById("bairro").innerHTML = "Bairro: " +response.bairro;
-            //document.getElementById("localidade").innerHTML = "Cidade: " +response.localidade;
-            //document.getElementById("uf").innerHTML = "Estado: " +response.uf;
-        }
-    })
->>>>>>> f7ee967bf7aa38f99adac1cc47dba5810433b733
 }
